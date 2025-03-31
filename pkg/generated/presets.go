4059e3053f58dfe604dce5809cba1ea61d7127a9
Andrei Kvapil <kvapss@gmail.com>
2025-03-31 09:48:01 +0200

    Update Cozystack templates (#42)
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- pkg/generated/presets.go
+++ pkg/generated/presets.go
@@ -15,7 +15,7 @@ templateOptions:
   fileValues: []
   jsonValues: []
   literalValues: []
-  talosVersion: "v1.8"
+  talosVersion: "v1.9"
   withSecrets: "secrets.yaml"
   kubernetesVersion: ""
   full: false
@@ -135,7 +135,7 @@ cluster:
 	"cozystack/values.yaml": `endpoint: "https://192.168.100.10:6443"
 clusterDomain: cozy.local
 floatingIP: 192.168.100.10
-image: "ghcr.io/aenix-io/cozystack/talos:v1.9.3"
+image: "ghcr.io/cozystack/cozystack/talos:v1.9.3"
 podSubnets:
 - 10.244.0.0/16
 serviceSubnets:

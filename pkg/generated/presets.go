1154eb6d563a9efc1f4a8181ae33b1a1ca52e982
Andrei Kvapil <kvapss@gmail.com>
2025-04-17 22:30:28 +0200

    Update Talos Linux image for Cozystack (#54)
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- pkg/generated/presets.go
+++ pkg/generated/presets.go
@@ -136,7 +136,7 @@ cluster:
 	"cozystack/values.yaml": `endpoint: "https://192.168.100.10:6443"
 clusterDomain: cozy.local
 floatingIP: 192.168.100.10
-image: "ghcr.io/cozystack/cozystack/talos:v1.9.3"
+image: "ghcr.io/cozystack/cozystack/talos:v1.9.5"
 podSubnets:
 - 10.244.0.0/16
 serviceSubnets:
